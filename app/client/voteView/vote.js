676b087411fc9245f81be583932ffd4762cbfc86
Rustin Crandall <rustinpc@gmail.com>
2014-12-09 20:15:02 -0800

    Setup vote view files. Still need some voting functionality work on the js file.

---
+++ app/client/voteView/vote.js
@@ -0,0 +1,85 @@
+/**
+ * Photo View Helpers  
+ */
+
+Template.vote.helpers({
+  captions: function() {
+    return Captions.find({});
+  },
+  getImage: function() {
+    var picId = Games.find({});
+    return Photos.find({_id: picId.photoId});
+  }
+});
+
+Template.vote.events({
+  'click .navigate-events': function(evt, template) {
+    Router.go('/game');
+  },
+
+  'click .go-back': function(evt, template) {
+    Router.go('/game');
+  },
+
+  'click .logout': function(evt, template) {
+    console.log('Logging user out...');
+    Meteor.logout(function(err) {
+      Router.go('/');
+    });
+  }
+});
+
+var hasUpVoted = function(voterId, photoId) {
+  var query = {'_id':photoId, 'upVotes': { $in: [ voterId ] } };
+  var voteCheck = Images.findOne(query);
+  return (voteCheck !== undefined);
+};
+
+var hasDownVoted = function(voterId, photoId) {
+  var query = {'_id':photoId, 'downVotes': { $in: [ voterId ] } };
+  var voteCheck = Images.findOne(query);
+  return (voteCheck !== undefined);
+};
+
+/**
+ * Helpers for each Photo
+ */
+
+Template.onecaption.events({
+  'click div.upvote': function(evt, template) {
+    var userId = Meteor.userId();
+    
+    var upVoteCheck = hasUpVoted(userId, this._id);
+    var downVoteCheck = hasDownVoted(userId, this._id);
+    if (downVoteCheck) {
+      Meteor.call('imagesUpsert', this._id, {$inc: {'voteCount': 1}});
+      Meteor.call('imagesUpsert', this._id, {$pull: {'downVotes': userId}});
+      console.log('removed downvote');
+    } else if (!upVoteCheck) {
+      console.log('upVoted');
+      Meteor.call('imagesUpsert', this._id, {$inc: {'voteCount': 1}});
+      Meteor.call('imagesUpsert', this._id, {$push: {'upVotes': userId}});
+    }
+    else {
+      console.log('prevented upvote');
+    }
+  },
+
+  'click div.downvote': function(evt, template) {
+    var userId = Meteor.userId();
+    
+    var upVoteCheck = hasUpVoted(userId, this._id);
+    var downVoteCheck = hasDownVoted(userId, this._id);
+    if (upVoteCheck) {
+      Meteor.call('imagesUpsert', this._id, {$inc: {'voteCount': -1}});
+      Meteor.call('imagesUpsert', this._id, {$pull: {'upVotes': userId}});
+      console.log('removed downvote');
+    } else if (!downVoteCheck) {
+      console.log('downvoted');
+      Meteor.call('imagesUpsert', this._id, {$inc: {'voteCount': -1}});
+      Meteor.call('imagesUpsert', this._id, {$push: {'downVotes': userId}});
+    } else {
+      console.log('prevented upvote');
+    }
+  }
+});
