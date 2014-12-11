1d46056ad30ea165fee230ae98bbd28db3e01f23
Sebastian Tonkin <sgtonkin@gmail.com>
2014-12-10 20:16:31 -0800

    Finished basic results page + styling

---
+++ app/client/resultsView/results.js
@@ -0,0 +1,46 @@
+/* global Captions, Photos, Games */
+Players = new Meteor.Collection('players');
+Photos = new Meteor.Collection('photos');
+Captions = new Meteor.Collection('captions');
+Games = new Meteor.Collection('games');
+
+var sortCaptions = function(a, b) {
+  return (b.upvoteCount - b.downvoteCount) - (a.upvoteCount - a.downvoteCount);
+}
+
+var getWinners = function() {
+  var captions = Captions.find({}).fetch();
+  captions.sort(sortCaptions);
+  return captions;
+}
+
+var getScore = function(caption) {
+  return caption.upvoteCount - caption.downvoteCount;
+}
+
+var getName = function(playerID) {
+  var player = Players.findOne({playerID:playerID});
+  return player.name;
+}
+
+Template.results.helpers({
+  winner: function() {
+    var winner = getWinners()[0];
+    return {
+      name: getName(winner.playerID),
+      text: winner.text,
+      score: getScore(winner)
+    }
+  },
+  runnersUp: function() {
+    //console.log(getWinners().slice(1,5));
+    //return Captions.find({});
+    return getWinners().slice(1,5);
+  },
+  photoPath: function() {
+    var photo = Games.find({}).fetch();
+    var photoID = photo[0].photoID;
+    var photoPath = Photos.find({photoID:photoID}).fetch();
+    return "/img/"+photoPath[0].path;
+  }
+});
