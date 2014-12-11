a17904acae3f07c01de9fdc73445267ff0ee270b
Sebastian Tonkin <sgtonkin@gmail.com>
2014-12-11 09:25:22 -0800

    Fixed jscs errors

--- app/client/resultsView/results.js
+++ app/client/resultsView/results.js
@@ -1,21 +1,21 @@
-/* global Captions: true, Template: true, Photos: true, Players: true, Games: true */
+/* global Captions: true, Template, Photos: true, Players: true, Games: true */
 Players = new Meteor.Collection('players');
 Photos = new Meteor.Collection('photos');
 Captions = new Meteor.Collection('captions');
 Games = new Meteor.Collection('games');
 
+var getScore = function(caption) {
+  return caption.upvoteCount - caption.downvoteCount ;
+};
+
 var sortCaptions = function(a, b) {
-  return (b.upvoteCount - b.downvoteCount) - (a.upvoteCount - a.downvoteCount);
+  return getScore(b) - getScore(a);
 };
 
 var getWinners = function() {
   var captions = Captions.find({}).fetch();
   captions.sort(sortCaptions);
   return captions;
-};
-
-var getScore = function(caption) {
-  return caption.upvoteCount - caption.downvoteCount;
 };
 
 var getName = function(playerID) {
@@ -33,8 +33,8 @@ Template.results.helpers({
     };
   },
   runnersUp: function() {
-    var runnersUp = getWinners().slice(1,5);
-    for ( var i = 0; i < runnersUp.length; i++) {
+    var runnersUp = getWinners().slice(1, 5);
+    for (var i = 0; i < runnersUp.length; i++) {
       runnersUp[i].name = getName(runnersUp[i].playerID);
     }
     return runnersUp;
@@ -43,6 +43,6 @@ Template.results.helpers({
     var photo = Games.find({}).fetch();
     var photoID = photo[0].photoID;
     var photoPath = Photos.find({photoID:photoID}).fetch();
-    return '/img/'+photoPath[0].path;
+    return '/img/' + photoPath[0].path;
   }
 });
