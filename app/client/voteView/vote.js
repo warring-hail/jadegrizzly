76845da8c0a90c2f7a3550810709c4b552d93c0d
Rustin Crandall <rustinpc@gmail.com>
2014-12-11 19:03:03 -0800

    Moved tracker function that forces state change for players to master file.

--- app/client/voteView/vote.js
+++ app/client/voteView/vote.js
@@ -103,18 +103,3 @@ Template.onecaption.events({
     }
   }
 });
-
-
-Tracker.autorun(function() {
-  var gameData = Games.findOne();
-  var host = Session.get('host');
-  var statePaths = ['pending', 'input', 'vote', 'results'];
-
-  if (gameData) {
-    var stateNum = gameData.stateID;
-    if (!host) {
-      Router.go('/' + statePaths[stateNum]);
-    }
-  }
-});
-
